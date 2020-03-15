79e22a41fd421694895c036edff338876573c220
joreiyo <62222850+joreiyo@users.noreply.github.com>
2020-03-15 19:58:31 -0400

    change to single image

--- js/app.js
+++ js/app.js
@@ -1,43 +1,11 @@
 const container = document.querySelector(".container");
 const coffees = [
   {
-    name: "Perspiciatis",
-    image: "images/coffee1.jpg"
-  },
-  {
-    name: "Voluptatem",
-    image: "images/coffee2.jpg"
-  },
-  {
     name: "Explicabo",
     image: "images/coffee3.jpg"
-  },
-  {
-    name: "Rchitecto",
-    image: "images/coffee4.jpg"
-  },
-  {
-    name: " Beatae",
-    image: "images/coffee5.jpg"
-  },
-  {
-    name: " Vitae",
-    image: "images/coffee6.jpg"
-  },
-  {
-    name: "Inventore",
-    image: "images/coffee7.jpg"
-  },
-  {
-    name: "Veritatis",
-    image: "images/coffee8.jpg"
-  },
-  {
-    name: "Accusantium",
-    image: "images/coffee9.jpg"
   }
 ];
-const showCoffees = () => {
+const showPicture = () => {
   let output = "";
   coffees.forEach(
     ({ name, image }) =>
@@ -45,14 +13,14 @@ const showCoffees = () => {
               <div class="card">
                 <img class="card--avatar" src=${image} />
                 <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
+                <a class="card--link" href="#">Save</a>
               </div>
               `)
   );
   container.innerHTML = output;
 };
 
-document.addEventListener("DOMContentLoaded", showCoffees);
+document.addEventListener("DOMContentLoaded", showPicture);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
