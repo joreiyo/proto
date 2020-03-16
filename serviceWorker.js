0667bfa67a1b33e00d1e73bf7d6340688633764b
joreiyo <62222850+joreiyo@users.noreply.github.com>
2020-03-15 20:07:40 -0400

    Clear Cache

--- serviceWorker.js
+++ serviceWorker.js
@@ -7,6 +7,23 @@ const assets = [
   "/images/coffee3.jpg"
 ];
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(staticDevCoffee).then(cache => {
@@ -22,3 +39,4 @@ self.addEventListener("fetch", fetchEvent => {
     })
   );
 });
+
